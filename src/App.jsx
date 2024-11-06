9e3f222a886388cb6146cd6b4cfffb7e5202e1a0
SamsonBarnabasDev <samsonbarnabas2002@gmail.com>
2024-11-07 03:15:02 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
+
 import Features from "./components/Features";
 
 import * as Sentry from "@sentry/react";
